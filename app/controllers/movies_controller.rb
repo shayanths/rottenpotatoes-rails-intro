8d331df9ebfeaf550c556841ef5eedd80d0a2edc
shayanths <ssinnarajah@berkeley.edu>
2016-02-17 06:51:23 +0000

    Finished Part 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,24 +10,58 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-
+  
+  
+  
   
 
   def index
-    @all_ratings = ['G', 'PG', 'PG-13', 'R']
-    @selected_ratings = (params[:ratings].present? ? params[:ratings] : [])
-    
-    
-    ratings_dict =  params['ratings']
 
-    if params.length == 0
-      @movies = Movie.all()
-    elsif (!ratings_dict.nil?)
-      @movies = Movie.where(rating: ratings_dict.keys)
-    else
-      @movies = Movie.order(params[:sort])
+    if params[:sort].nil? && params[:ratings].nil? && (!session[:sort].nil? || !session[:ratings].nil?)
+      redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
     end
+
+    @sort_values = params[:sort]
+    @ratings_values = params[:ratings] 
+    if @ratings_values.nil?
+      ratings_values = Movie.ratings 
+    else
+      ratings_values = @ratings_values.keys
+    end
+
+    @all_ratings = Movie.ratings.inject(Hash.new) do |all_ratings, rating|
+          all_ratings[rating] = @ratings_values.nil? ? false : @ratings_values.has_key?(rating) 
+          all_ratings
+      end
+      
+    if !@sort_values.nil?
+        @movies = Movie.order("#{@sort_values} ASC").where(rating: ratings_values)
+    else
+      @movies = Movie.where(rating: ratings_values)
+    end
+
+    session[:sort] = @sort_values
+    session[:ratings] = @ratings_values
   end
+
+
+  
+
+  # def index
+  #   @all_ratings = ['G', 'PG', 'PG-13', 'R']
+  #   @selected_ratings = (params[:ratings].present? ? params[:ratings] : [])
+    
+    
+  #   ratings_dict =  params['ratings']
+
+  #   if params.length == 0
+  #     @movies = Movie.all()
+  #   elsif (!ratings_dict.nil?)
+  #     @movies = Movie.where(rating: ratings_dict.keys)
+  #   else
+  #     @movies = Movie.order(params[:sort])
+  #   end
+  # end
 
   def new
     # default: render 'new' template
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,14 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  def helper_class(type_help)
+    if (params[:sort].to_s == type_help)
+      return 'hilite'
+    else
+      return nil
+    end
+  end
+    
+      
+  
 end
